da6285a8404355524ff68c37ea64d6b70d5d5c6e
Emmanuel Kibicho <emmanuel.kibicho@outlook.com>
2020-05-10 01:31:09 -0400

    Added wrapper function to eliminate redundancies

--- Chapter 7/Fibo.cpp
+++ Chapter 7/Fibo.cpp
@@ -6,6 +6,7 @@ const int MIN_INDEX = 0; //1st term
 const int MAX_INDEX = 20; //last term
 
 int fib(int n);//Function prototype
+int additiveSequence(int n, int t0, int t1);
 
 int main() {
    cout << "THis is the fibonacci sequence\n";
@@ -18,11 +19,13 @@ int main() {
    
 //Function recursively calls fibonacci sequence
 int fib(int n) {
-//base case
- if(n < 2) {
-   return 1;
- } else {
-   return fib(n-1) + fib (n-2);
-    }
+return additiveSequence(n , 0, 1);
+}
 
+//wrapper function to eliminate redundant recursive calls
+int additiveSequence(int n, int t0, int t1) {
+ if ( n == 0 ) return t0;
+ if ( n == 1 ) return t1;
+ 
+ return additiveSequence( n - 1, t1, t0 + t1);
 }
